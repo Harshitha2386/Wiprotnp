524175ea604f29b8c5ab76b06fe4aa0a3d8204b4
Harshitha2386 <harshithatss23@gmail.com>
2026-07-03 23:04:35 +0530

    Initial commit

---
+++ java_fundamentals/src/languagebasics/HandsOnAssignment3.java
@@ -0,0 +1,10 @@
+package languagebasics;
+
+public class HandsOnAssignment3 {
+	public static void main(String[] args){
+        int n1 = Integer.parseInt(args[0]);
+        int n2 = Integer.parseInt(args[1]);
+        int sum = n1+n2;
+        System.out.println("The sum of "+n1+ " and "+n2+" is "+sum);
+    }
+}
