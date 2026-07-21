74c03ac2ab3b8bcc543f4c3f839c38328d775a5d
Harshitha2386 <harshithatss23@gmail.com>
2026-07-21 21:58:25 +0530

    Add README file

--- JUnit/src/Assert_Methods_and_Annotations/HandsOnAssignment1/com/wipro/task/SortValues.java
+++
@@ -1,9 +0,0 @@
-package Assert_Methods_and_Annotations.HandsOnAssignment1.com.wipro.task;
-import java.util.Arrays;
-
-public class SortValues {
-	public int[] sortValues(int[] arr) {
-        Arrays.sort(arr);
-        return arr;
-    }
-}
--- JUnit/src/Assert_Methods_and_Annotations/HandsOnAssignment2/com/wipro/task/SortValues.java
+++
@@ -1,8 +0,0 @@
-package Assert_Methods_and_Annotations.HandsOnAssignment2.com.wipro.task;
-import java.util.Arrays;
-public class SortValues {
-	public int[] sortValues(int[] arr) {
-        Arrays.sort(arr);
-        return arr;
-    }
-}
